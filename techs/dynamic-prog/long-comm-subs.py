82049a4b0dd0576a5e5d473d61220198cd9fbb57
xqiu <x.qiu@qq.com>
2020-04-04 14:37:43 +0800

    - add longest common subsequence

--- techs/dynamic-prog/long-comm-subs.py
+++ techs/dynamic-prog/long-comm-subs.py
@@ -2,15 +2,16 @@ import numpy as np
 
 
 def longest_common_subsequence(x, y):
+    """ 根据递归式计算最长公共子序列
+    """
     m, n = len(x), len(y)
     # 把x,y中字符的下标向右移一位(方便计算)
     x1 = ' ' + x
     y1 = ' ' + y
     # c[m][n]为最大公共子序列的长度
     c = np.zeros((m+1, n+1))  # 初始化c
-    # 记录公共子序列
+    # 记录下标改变的路径(用字符串表示)
     b = np.zeros((m+1, n+1)).tolist()
-
     for i in range(1, m+1):
         for j in range(1, n+1):
             if x1[i] == y1[j]:
@@ -26,6 +27,8 @@ def longest_common_subsequence(x, y):
 
 
 def get_common_subsequence(x1, y1, b):
+    """ 根据b还原最长公共子序列
+    """
     i, j = len(x1) - 1, len(y1) - 1
     res = []
     while i and j:
