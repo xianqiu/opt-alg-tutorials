30f5351a0eda63ab2b55e3d51ebb656b7e463c54
xianqiu <x.qiu@qq.com>
2025-06-28 11:25:07 +0800

    local search

--- codes/search/local_search.py
+++ codes/search/local_search.py
@@ -1,46 +1,6 @@
----
-weight: 230
-title: "Local Search"
-description: ""
-icon: "article"
-date: "2025-06-24T19:58:58+08:00"
-lastmod: "2025-06-24T19:58:58+08:00"
-draft: false
-toc: true
-katex: true
----
-
-Local search is a simple but powerful algorithm. The basic idea is to start with a solution and then iteratively improve it. The improvement is done by trying out different moves. The move that results in the best improvement is chosen.
-
-We use the traveling salesman problem (TSP) as an example to show the power of local search algorithm.
-
-## TSP Problem
-
-Given a complete graph `G = (V, E)` with edge weights `d(e)` for each edge `e ∈ E`. Each node represents a city and the edge weights represent the distances between cities. The TSP problem is to find the shortest route that visits all the cities exactly once. 
-
-{{< figure src="tsp.png" width="300px" class="text-center">}}
-
-## 2-OPT
-
-Now we introduce a local search algorithm called `2-OPT` to solve the TSP problem. The algorithm is as follows:
-
-1. Given a feasible initial solution $s$. Let us denote it by $s=\{(i_1, i_2), (i_2, i_3), \ldots, (i_n, i_1)\}$.
-2. Define the neighborhood $\delta(s)$ of $s$: For any two arcs $(u, v)$ and $(s, t)$ in $s$, let us consider the solution $s'$ obtained by swapping their connections, i.e., $(u, s)$ and $(v, t)$. 
-    {{< figure src="2opt.png" width="600px" class="text-center">}}
-3. Search for $s'\in \delta(s)$, if $d(s') < d(s)$ (where $d(s)$ represents the total length of $s$), then take $s'$ as the new solution.
-4. Stop when no improvement can be made or when the maximum number of iterations is reached.
-
-* Example
-
-{{< figure src="example.gif" width="600px" class="text-center">}}
-
-## Code
-
-```python
-
 class TSP2opt(object):
 
-    max_iter = 10000
+    max_iter = 1000
 
     def __init__(self, d):
         """
@@ -94,18 +54,11 @@ class TSP2opt(object):
             is_improved = current_length - self.tour_length > 0
 
         return self
-```
-
-We may use it in the following way.
-
-```python
-import numpy as np
 
 
 if __name__ == '__main__':
-    # Generate a random TSP instance.
+    import numpy as np
     n = 100  # number of cities
     d = np.random.randint(1, 1000, (n, n))
     tsp = TSP2opt(d)
     tsp.solve()
-```
